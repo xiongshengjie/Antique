dbd590b460926fadb5341491711774dd1c6d9b11
DeathDream <DeathDream@DESKTOP-CN95LT6>
2017-05-21 20:55:29 +0800

    user

--- src/com/antique/domain/User.java
+++ src/com/antique/domain/User.java
@@ -4,6 +4,15 @@ public class User {
 
 	private String name;
 	private String password;
+	private String role;
+	
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 	public String getName() {
 		return name;
 	}
