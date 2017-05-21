940e8e446c02933b53d94960bf71acc8502b9774
Administrator <Administrator@ASUS>
2017-05-21 21:00:21 +0800

    xml

--- src/com/antique/dao/AntiqueDao.java
+++ src/com/antique/dao/AntiqueDao.java
@@ -1,5 +1,52 @@
 package com.antique.dao;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.antique.domain.Antique;
+
 public class AntiqueDao {
 
+	public List<Antique> listAll() throws DocumentException {
+		SAXReader reader = new SAXReader();
+		
+		List<Antique> list = new ArrayList<Antique>();
+		Antique antique;
+		Document document = reader.read(new File("src/antique.xml"));
+		
+		Element root = document.getRootElement();
+		List<Element> antiqueList = root.elements("antique");
+		
+		for(Element element : antiqueList){
+			Attribute id = element.attribute("id");
+			Attribute name = element.attribute("name");
+			Attribute category = element.attribute("category");
+			
+			Element size = element.element("size");
+			Element createtime = element.element("createtime");
+			Element findtime = element.element("findtime");
+			Element findplace = element.element("findplace");
+			Element description = element.element("description");
+			Element imgurl = element.element("imgurl");
+			
+			antique = new Antique(id.getStringValue(),name.getStringValue(),category.getStringValue(),
+											size.getStringValue(),createtime.getStringValue(),findtime.getStringValue(),
+											findplace.getStringValue(),description.getStringValue(),imgurl.getStringValue());
+			list.add(antique);
+		}
+		
+		return list;
+	}
+	
+	public void add(Antique antique) {
+		
+	}
+	
 }
--- src/com/antique/domain/Antique.java
+++ src/com/antique/domain/Antique.java
@@ -1,5 +1,88 @@
 package com.antique.domain;
 
 public class Antique {
-
+	
+	private String id;
+	private String name;
+	private String category;
+	private String size;
+	private String createtime;
+	private String findtime;
+	private String findplace;
+	private String description;
+	private String imgurl;
+	
+	
+	public Antique() {
+		super();
+	}
+	public Antique(String id, String name, String category, String size, String createtime, String findtime,
+			String findplace, String description, String imgurl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.size = size;
+		this.createtime = createtime;
+		this.findtime = findtime;
+		this.findplace = findplace;
+		this.description = description;
+		this.imgurl = imgurl;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+	public String getFindtime() {
+		return findtime;
+	}
+	public void setFindtime(String findtime) {
+		this.findtime = findtime;
+	}
+	public String getFindplace() {
+		return findplace;
+	}
+	public void setFindplace(String findplace) {
+		this.findplace = findplace;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImgurl() {
+		return imgurl;
+	}
+	public void setImgurl(String imgurl) {
+		this.imgurl = imgurl;
+	}
+	
+	
 }
