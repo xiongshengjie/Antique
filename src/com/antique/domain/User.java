26aad9b76dd95787f8220a55d48b4e02363aeea6
Administrator <Administrator@ASUS>
2017-05-21 21:04:31 +0800

    1

--- src/com/antique/domain/User.java
+++ src/com/antique/domain/User.java
@@ -2,4 +2,29 @@ package com.antique.domain;
 
 public class User {
 
+	private String name;
+	private String password;
+	private String role;
+	
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
